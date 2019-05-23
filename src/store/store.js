1ed0ba894eaf8a711267e7bbfa7cb826df6f48a0
wangrui <836763392@qq.com>
2019-05-23 17:40:42 +0800

    init

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,17 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import * as home from './home/reducer'
 
+
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk)
+);
+
  const store = createStore(
     combineReducers({...home}),
-    applyMiddleware(thunk)
+     enhancer
 )
 export default store
