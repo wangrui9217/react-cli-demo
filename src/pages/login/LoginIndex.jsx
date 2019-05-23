11399da1fa7bbc2fe32bfa436ea10be279d2db50
wangrui <836763392@qq.com>
2019-05-23 15:44:17 +0800

    first commit

---
+++ src/pages/login/LoginIndex.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class HomeIndex extends Component{
+
+    render () {
+        return (
+            <div>
+                <p>111111111</p>
+            </div>
+        )
+    }
+
+}
